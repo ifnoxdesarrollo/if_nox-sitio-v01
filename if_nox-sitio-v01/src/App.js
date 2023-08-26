c27651050052f38ba3131c517e181b65c75de3e2
Evaristo.Aguirre <dasbrotchen@gmail.com>
2023-08-26 11:28:10 -0300

    desarrollo-asi-trabajamos

--- if_nox-sitio-v01/src/App.js
+++ if_nox-sitio-v01/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Nuestros_Servicios from './componentes/nuestros servicios/Nuestros_Servicios';
+import { S5_Asi_trabajamos } from './Componentes/S5_Asi_trabajamos/S5_Asi_trabajamos';
+import Nuestros_Servicios from './Componentes/nuestros_servicios/Nuestros_Servicios';
 
 function App() {
   return (
     <div className="App">
       <Nuestros_Servicios/>
+      <S5_Asi_trabajamos/>
     </div>
   );
 }
---
+++ if_nox-sitio-v01/src/Componentes/S5_Asi_trabajamos/S5_Asi_trabajamos.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import './S5_Asi_trabajamos.css';
+
+const S5_Asi_trabajamos = () => {
+    return(
+        <div className='caja-ppal'>
+            <h3 className='seccion__titulo'>Así trabajamos</h3>
+            <div className='container laberinto'>
+                    
+                    <div class="caja-1">
+                        <div className='caja-1-int'>
+                            <div className='textos-der'>
+                                <h3>Service</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="caja-2">
+                        <div className='caja-2-int'>
+                            <div className='textos-izq'>
+                                <h3>Service</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="caja-3">
+                        <div className='caja-3-int'>
+                            <div className='textos-der'>
+                                <h3>Service</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="caja-4">
+                        <div className='caja-4-int'>
+                            <div className='textos-izq'>
+                                <h3>Service</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="caja-5">
+                        <div className='caja-5-int'>
+                            <div className='textos-der'>
+                                <h3>Service</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="caja-6">
+                        <div class="caja-6-a"></div>
+                        <div class="caja-6-b">
+                        <h3>Service</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                    </div>
+                    <div class="caja-7">
+                        <div class="caja-7-a-0"></div>
+                        <div class="caja-7-b-0"></div>
+                        <div class="caja-7-ab"></div>
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export { S5_Asi_trabajamos }
