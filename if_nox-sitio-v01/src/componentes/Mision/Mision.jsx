e6fd49e93a802542dd45fa1638a8f55cf0d43f66
julieta548 <julietasosa548@gmail.com>
2023-08-30 12:44:20 -0300

    modificaciones varias

--- if_nox-sitio-v01/src/componentes/Mision/Mision.jsx
+++ if_nox-sitio-v01/src/componentes/Mision/Mision.jsx
@@ -7,12 +7,11 @@ function Mision() {
       <div className="mision__contenedor">
         <div className="mision__contenedor__textos">
           <h1 className='mision__contenedor__textos__titulo'>Nuestra Misión</h1>
-          <p className='mision__contenedor__textos__parrafo'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            usto a ratione exercitationem quae, officiis rerum eius temporibus
-            provident dignissimos rem debitis ut nesciunt repellendus
-            impedit autem illum quis ullam.</p>
+          <p className='mision__contenedor__textos__parrafo'>
+            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui fficia deserunt mollit anim id est laborum. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui fficia deserunt mollit anim id est laborum.
+          </p>
         </div>
-        <div className="mision__contenedor__grilla">
+        {/* <div className="mision__contenedor__grilla">
           <div className="mision__contenedor__grilla__fila-1">
             <img className='mision__contenedor__grilla__fila-1__imagen-1' src="/images/mision1.png" alt="" />
             <img className='mision__contenedor__grilla__fila-1__imagen-2' src="/images/mision2.png" alt="" />
@@ -20,7 +19,7 @@ function Mision() {
           <div className="mision__contenedor__grilla__fila-2">
             <img className='mision__contenedor__grilla__fila-1__imagen-3' src="/images/mision3.png" alt="" />
           </div>
-        </div>
+        </div> */}
       </div>
 
     </div>
