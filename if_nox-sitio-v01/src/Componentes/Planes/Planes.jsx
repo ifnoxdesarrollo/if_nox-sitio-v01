06617c4781748977067e43dca47bc6abfaabbffa
julieta548 <julietasosa548@gmail.com>
2023-08-31 18:20:59 -0300

    seccion planes

---
+++ if_nox-sitio-v01/src/Componentes/Planes/Planes.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+
+function Planes() {
+    return (
+        <div className='planes'>
+            <div className="planes__contenedor">
+                <div className="planes__contenedor__texto">
+                    <h2 className='planes__contenedor__texto__titulo'></h2>
+                    <h3 className='planes__contenedor__texto__subtitulo'></h3>
+                </div>
+                <div className="planes__contenedor__cards">
+                    <div className="planes__contenedor__cards__card">
+                        <div className="planes__contenedor__cards__card__bloque-1">
+                            <h2></h2>
+                            <h3></h3>
+                            <h2></h2>
+                            <h3></h3>
+                            <button></button>
+                        </div>
+                        <div className="planes__contenedor__cards__card__bloque-2">
+                            <h3></h3>
+                            <i></i><h3></h3>
+                            <i></i><h3></h3>
+                            <i></i><h3></h3>
+                        </div>
+                        <div className="planes__contenedor__cards__card">
+                            <div className="planes__contenedor__cards__card__bloque-1">
+                                <h2></h2>
+                                <h3></h3>
+                                <h2></h2>
+                                <h3></h3>
+                                <button></button>
+                            </div>
+                            <div className="planes__contenedor__cards__card__bloque-2">
+                                <h3></h3>
+                                <i></i><h3></h3>
+                                <i></i><h3></h3>
+                                <i></i><h3></h3>
+                            </div>
+                        </div>
+                        <div className="planes__contenedor__cards__card">
+                            <div className="planes__contenedor__cards__card__bloque-1">
+                                <h2></h2>
+                                <h3></h3>
+                                <h2></h2>
+                                <h3></h3>
+                                <button></button>
+                            </div>
+                            <div className="planes__contenedor__cards__card__bloque-2">
+                                <h3></h3>
+                                <i></i><h3></h3>
+                                <i></i><h3></h3>
+                                <i></i><h3></h3>
+                            </div>
+                        </div>
+                        <div className="planes__contenedor__cards__card">
+                            <div className="planes__contenedor__cards__card__bloque-1">
+                                <h2></h2>
+                                <h3></h3>
+                                <h2></h2>
+                                <h3></h3>
+                                <button></button>
+                            </div>
+                            <div className="planes__contenedor__cards__card__bloque-2">
+                                <h3></h3>
+                                <i></i><h3></h3>
+                                <i></i><h3></h3>
+                                <i></i><h3></h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Planes
