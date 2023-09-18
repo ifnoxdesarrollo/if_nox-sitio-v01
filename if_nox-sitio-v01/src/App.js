57a3fc2c07ce2f8a2bf4d695e42f0d602280c719
Agustin Alejandro Sosa <sagustin2001@gmail.com>
2023-09-18 16:09:09 -0300

    Nuestros servicios responsive

--- if_nox-sitio-v01/src/App.js
+++ if_nox-sitio-v01/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import Contacto from './componentes/Contacto/Contacto';
-import Footer from './componentes/Footer/Footer';
-import Hero from './componentes/Hero/Hero';
-import Mision from './componentes/Mision/Mision';
-import Proy_dest from './componentes/Proyectos Destacados/Proy_dest';
-import Nuestros_Servicios from './componentes/nuestros servicios/Nuestros_Servicios';
-import S5_Asi_trabajamos from './componentes/S5_Asi_trabajamos/S5_Asi_trabajamos';
-import Planes from './componentes/Planes/Planes';
-import Nos_presentamos from './componentes/Nos presentamos/Nos_presentamos';
-import Navbar from './componentes/Navbar/Navbar';
+import Contacto from './Componentes/Contacto/Contacto';
+import Footer from './Componentes/Footer/Footer';
+import Hero from './Componentes/Hero/Hero';
+import Mision from './Componentes/Mision/Mision';
+import Proy_dest from './Componentes/Proyectos Destacados/Proy_dest';
+import Nuestros_Servicios from './Componentes/nuestros servicios/Nuestros_Servicios';
+import S5_Asi_trabajamos from './Componentes/S5_Asi_trabajamos/S5_Asi_trabajamos';
+import Planes from './Componentes/Planes/Planes';
+import Nos_presentamos from './Componentes/Nos presentamos/Nos_presentamos.jsx';
+import Navbar from './Componentes/Navbar/Navbar';
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
       <Mision></Mision>
       <Nuestros_Servicios></Nuestros_Servicios>
       <Proy_dest></Proy_dest>
-      {/* <S5_Asi_trabajamos></S5_Asi_trabajamos> */}
+      <S5_Asi_trabajamos></S5_Asi_trabajamos>
       <Planes></Planes>
       <Nos_presentamos></Nos_presentamos>
       <Contacto></Contacto>
--- if_nox-sitio-v01/src/Componentes/Nos presentamos/Nos_presentamos.jsx
+++ if_nox-sitio-v01/src/Componentes/Nos presentamos/Nos_presentamos.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Nos_presentamos.css';
+// import './Nos_presentamos.js';
 
 function Nos_presentamos() {
     return (
         <div className='nosotros' id='nosotros'>
             <div className="nosotros__contenedor">
                 <div className="nosotros__contenedor__imagen-grande">
-                    <img className='nosotros__contenedor__imagen-grande__imagen' src="images/Pablo-olima.png" alt="" />
+                    <img id='nosotros1' className='nosotros__contenedor__imagen-grande__imagen' src="images/Pablo-olima.png" alt="" />
                     {/* <div className='imagen-texto'>
                         <h3 className='imagen-texto__nombre'>Pablo Olima</h3>
                         <p className='imagen-texto__puesto'>Director Creativo</p>
@@ -19,11 +20,11 @@ function Nos_presentamos() {
                     </div>
                     <div className="nosotros__contenedor__imagenes-chicas__imagenes">
                         <div className="contenedor-imagen-1">
-                            <img className='nosotros__contenedor__imagenes-chicas__imagenes__imagen-1' src="images/Julieta-sosa.png" alt="" />
+                            <img id='nosotros2' className='nosotros__contenedor__imagenes-chicas__imagenes__imagen-1' src="images/Julieta-sosa.png" alt="" />
                         </div>
                         <div className="contenedor-imagen-2">
-                            <img className='nosotros__contenedor__imagenes-chicas__imagenes__imagen-3' src="images/Agustin-sosa.png" alt="" />
-                            <img className='nosotros__contenedor__imagenes-chicas__imagenes__imagen-4' src="images/Evaristo-aguirre.png" alt="" />
+                            <img id='nosotros3' className='nosotros__contenedor__imagenes-chicas__imagenes__imagen-3' src="images/Agustin-sosa.png" alt="" />
+                            <img id='nosotros4' className='nosotros__contenedor__imagenes-chicas__imagenes__imagen-4' src="images/Evaristo-aguirre.png" alt="" />
                         </div>
                     </div>
                 </div>
