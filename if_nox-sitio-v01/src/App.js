8916476e56bd97f08c2c065971caf4de3b9ca374
julieta548 <julietasosa548@gmail.com>
2023-09-19 12:29:54 -0300

    asitrabajmos

--- if_nox-sitio-v01/src/App.js
+++ if_nox-sitio-v01/src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import Contacto from './componentes/Contacto/Contacto';
-import Footer from './componentes/Footer/Footer';
-import Hero from './componentes/Hero/Hero';
-import Mision from './componentes/Mision/Mision';
-import Proy_dest from './componentes/Proyectos Destacados/Proy_dest';
-import Nuestros_Servicios from './componentes/nuestros servicios/Nuestros_Servicios';
-import Planes from './componentes/Planes/Planes';
-import Nos_presentamos from './componentes/Nos presentamos/Nos_presentamos.jsx';
-import Navbar from './componentes/Navbar/Navbar';
-import Asi_trabajamos from './componentes/S5_Asi_trabajamos/Asi_trabajamos';
+import "./App.css";
+import Contacto from "./componentes/Contacto/Contacto";
+import Footer from "./componentes/Footer/Footer";
+import Hero from "./componentes/Hero/Hero";
+import Mision from "./componentes/Mision/Mision";
+import Proy_dest from "./componentes/Proyectos Destacados/Proy_dest";
+import Nuestros_Servicios from "./componentes/nuestros servicios/Nuestros_Servicios";
+import Planes from "./componentes/Planes/Planes";
+import Nos_presentamos from "./componentes/Nos presentamos/Nos_presentamos.jsx";
+import Navbar from "./componentes/Navbar/Navbar";
+import Asi_trabajamos from "./componentes/S5_Asi_trabajamos/asi_trabajamos";
 
 function App() {
   return (
@@ -19,6 +19,7 @@ function App() {
       <Nuestros_Servicios></Nuestros_Servicios>
       <Proy_dest></Proy_dest>
       <Asi_trabajamos></Asi_trabajamos>
+
       <Planes></Planes>
       <Nos_presentamos></Nos_presentamos>
       <Contacto></Contacto>
