4a6ca0e4b04fb42b5a12a41687628cebd71580db
julieta548 <julietasosa548@gmail.com>
2023-09-21 15:13:37 -0300

    arreglos mision

--- src/componentes/Mision/Mision.jsx
+++ src/componentes/Mision/Mision.jsx
@@ -8,7 +8,9 @@ function Mision() {
         <div className="mision__contenedor__textos">
           <h1 className='mision__contenedor__textos__titulo'>Nuestra Misión</h1>
           <p className='mision__contenedor__textos__parrafo'>
-            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui fficia deserunt mollit anim id est laborum. Neque porro
+            Nuestra misión es impulsar el potencial de tu presencia en línea. Nos dedicamos a crear soluciones web innovadoras y efectivas que no solo cumplen con tus necesidades comerciales, sino que también superan tus expectativas. Creemos en la importancia de la excelencia técnica, la creatividad y la atención al detalle en cada proyecto que emprendemos. <br />
+            <br />
+            Nuestro objetivo es brindarte no solo un sitio web, sino una poderosa herramienta que te ayude a alcanzar tus metas comerciales y a conectarte de manera significativa con tu audiencia. Con pasión y compromiso, trabajamos incansablemente para hacer que tu visión digital se convierta en una realidad.
           </p>
         </div>
       </div>
