9961cf44d885bba615c34e19bcf451d17ca18738
julieta548 <julietasosa548@gmail.com>
2023-09-25 15:53:13 -0300

    cambios juli

--- src/componentes/Contacto/Contacto.jsx
+++ src/componentes/Contacto/Contacto.jsx
@@ -8,7 +8,7 @@ function Contacto() {
         <div className="contacto__contenedor__bloque">
           <h3 className='contacto__contenedor__bloque__titulo'>Podemos asesorarte ¡Contanos qué necesitás!</h3>
           <div className="contacto__contenedor__bloque__iconos">
-            <a href="https://wa.me/+54926123123123" target='_blank'><img className='contacto__contenedor__bloque__iconos__icono-1' src="/images/whatsapp.png" alt="whatsapp" /></a>
+            <a href="https://api.whatsapp.com/send?phone=542613393663&text=Hola%20If%20Nox,%20quiero%20conocer%20más%20sobre%20los%20servicios%20que%20ofrecen..." target='_blank'><img className='contacto__contenedor__bloque__iconos__icono-1' src="/images/whatsapp.png" alt="whatsapp" /></a>
             <a href=""><img className='contacto__contenedor__bloque__iconos__icono-2' src="/images/calendario.png" alt="calendario" /></a>
             <a href="mailto:ifnox360@gmail.com" target='_blank'><img className='contacto__contenedor__bloque__iconos__icono-3' src="/images/mail.png" alt="email" /></a>
             <a href="https://www.instagram.com/ifnoxdesarrolloweb" target='_blank'><img className='contacto__contenedor__bloque__iconos__icono-4' src="/images/ig.png" alt="instagram" /></a>
