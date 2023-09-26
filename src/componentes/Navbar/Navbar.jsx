896991dc62712d69091bf0ae30ade3a37d6e8c37
Agustin Alejandro Sosa <sagustin2001@gmail.com>
2023-09-26 13:32:14 -0300

    navbar responsive

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -3,39 +3,52 @@ import './Navbar.css'
 
 function Navbar() {
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const menuToggle = document.getElementById('menuToggle');
-        const menu = document.getElementById('menu');
+    const menuToggle = () => {
+        const menu = document.getElementById('navbarNav');
+        const contenedor = document.getElementById('navContenedor');
+        const navItem1 = document.getElementById('navItem1');
+        const navItem2 = document.getElementById('navItem2');
+        const navItem3 = document.getElementById('navItem3');
+        contenedor.classList.toggle('opened');
 
-        menuToggle.addEventListener('click', function () {
+        if (contenedor.classList.contains('opened')) {
+            setTimeout(function () {
+                navItem1.classList.toggle('opened');
+                navItem2.classList.toggle('opened');
+                navItem3.classList.toggle('opened');
+                menu.classList.toggle('active');
+            }, 200);
+        } else {
+            navItem1.classList.toggle('opened');
+            navItem2.classList.toggle('opened');
+            navItem3.classList.toggle('opened');
             menu.classList.toggle('active');
-        });
-    });
+        }
+    }
 
     return (
-        <div className='navbar'>
+        <div className='navbar' id="navContenedor">
             <div className="navbar__contenedor">
                 <div className="navbar__contenedor__bloque-1">
                     <a className="navbar-marca" href="#">IF NOX</a>
-                    {/* <img className='switch' src="images/Toggle.png" alt="" />
+                    <img className='switch' src="images/Toggle.png" alt="" />
                     <div className='llamada'>
                         <h5>Cambiá al modo oscuro</h5>
-                    </div> */}
+                    </div>
                 </div>
-                <div class="menu-toggle" id="menuToggle">
-                    <div class="bar"></div>
-                    <div class="bar"></div>
-                    <div class="bar"></div>
+                <div className="menu-toggle" id="menuToggle" onClick={menuToggle}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
                 </div>
                 <div className="navbar__contenedor__bloque-2" id="navbarNav">
-
-                    <div className="nav-item-1">
+                    <div className="nav-item-1" id='navItem1'>
                         <a className="nav-link" href="#planes">Planes</a>
                     </div>
-                    <div className="nav-item-2">
+                    <div className="nav-item-2" id='navItem2'>
                         <a className="nav-link" href="#nosotros">Nosotros</a>
                     </div>
-                    <div className="nav-item-3">
+                    <div className="nav-item-3" id='navItem3'>
                         <a className="nav-link" href="#contacto">Contacto</a>
                     </div>
                 </div>
